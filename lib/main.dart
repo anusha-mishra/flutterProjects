5d58fe1f692f3e8b2ba6a7b2e8747daeded5eea7
anusha-mishra <anushabgp017@gmail.com>
2023-03-02 00:50:22 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/notification.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:firebase_core/firebase_core.dart';
+//import 'package:firebase_messaging/firebase_messaging.dart';
+//import 'package:firebase_core/firebase_core.dart';
 
-Future<void> backgroundHandler(RemoteMessage message) async{
-  print(message.data.toString());
-  print(message.notification!.title);
-}
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  FirebaseMessaging.onBackgroundMessage(backgroundHandler);
+void  main() {
   runApp(const MyApp());
+  
 }
+// Future<void> backgroundHandler(RemoteMessage message) async{
+//   print(message.data.toString());
+//   print(message.notification!.title);
+// }
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp();
+//   FirebaseMessaging.onBackgroundMessage(backgroundHandler);
+//   runApp(const MyApp());
+// }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
